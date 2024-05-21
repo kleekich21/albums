23ad5fdcaa0aadd193a4a2faee27194f16b8e677
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-05-21 17:41:08 +0900

    feat: render loading, error, list UI conditionally for pending, rejected, fullfiled cases of thunk actions

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,15 +1,32 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
 
 function UsersList() {
   const dispatch = useDispatch();
+  const { isLoading, data, error } = useSelector((state) => {
+    return state.users;
+  });
 
   useEffect(() => {
     dispatch(fetchUsers());
   }, []);
 
-  return "Users List";
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error fetching data...</div>;
+  }
+
+  return (
+    <div key={data}>
+      {data.map((user) => (
+        <div>{user.name}</div>
+      ))}
+    </div>
+  );
 }
 
 export default UsersList;
